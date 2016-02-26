139e34593d249394585082a1d3135014d8449ed7
José Armesto <github@armesto.net>
2016-02-26 10:50:47 +0100

    Improved test

--- test/LoginUseCaseTest.php
+++ test/LoginUseCaseTest.php
@@ -3,9 +3,10 @@ class LoginUseCaseTest extends PHPUnit_Framework_TestCase
 {
 	public function setUp()
     {
+        $this->session 		= $this->getMock( 'SessionInterface' );
         $this->user_repo 		= $this->getMock( 'UserRepository' );
         $this->password_encoder = $this->getMock( 'PasswordEncoder' );
-        $this->use_case 		= new LoginUseCase( $this->user_repo, $this->password_encoder );
+        $this->use_case 		= new LoginUseCase( $this->session, $this->user_repo, $this->password_encoder );
 
         $this->sample_user 		= new \MyApp\Entity\User( 'fiunchinho', 'Jose', 'jose@armesto.net', 'encoded password' );
         $this->correct_password = 'password';
@@ -13,55 +14,24 @@ class LoginUseCaseTest extends PHPUnit_Framework_TestCase
 
 	public function testItShouldReturnUserLoginResponse()
     {
-        // Arrange
-    	$request_parameters = array(
-            'email' 	=> $this->sample_user->getEmail(),
-            'password' 	=> $this->correct_password,
-        );
-        $request = new UserLoginRequest( $request_parameters );
+        $request = new UserLoginRequest($this->sample_user->getEmail(), $this->sample_user->getPassword() );
 
-        // Act
         $response = $this->use_case->execute( $request );
 
-        // Assert
         $this->assertInstanceOf( 'UserLoginResponse', $response );
-    }
-
-    public function testItShouldIncludeUserInUserLoginResponse()
-    {
-        // Arrange
-        $request_parameters = array(
-            'email' 	=> $this->sample_user->getEmail(),
-            'password' 	=> $this->correct_password,
-        );
-        $request = new UserLoginRequest( $request_parameters );
-
-        // Act
-        $response = $this->use_case->execute( $request );
-
-        // Assert
-        $this->assertInstanceOf( '\MyApp\Entity\User', $response->user );
     }
 
     public function testItShouldThrowOnIncorrectPassword()
     {
-        $request_parameters = array(
-            'email' 	=> $this->sample_user->getEmail(),
-            'password' 	=> "Some wrong password",
-        );
-        $request = new UserLoginRequest( $request_parameters );
+        $request = new UserLoginRequest($this->sample_user->getEmail(), "Some wrong password");
 
         $this->setExpectedException( 'IncorrectPasswordException' );
-        $response = $this->use_case->execute( $request );
+        $this->use_case->execute($request);
     }
 
     public function testItShouldThrowIfUserNotFound()
     {
-        $request_parameters = array(
-            'email' => 'unknown@example.com',
-            'password' => $this->correct_password,
-        );
-        $request = new UserLoginRequest( $request_parameters );
+        $request = new UserLoginRequest('unknown@example.com', $this->correct_password);
 
         $this->setExpectedException( 'UserNotFoundException' );
         $response = $this->use_case->execute( $request );
